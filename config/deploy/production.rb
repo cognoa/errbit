665af3e395177d794e82cc21bc1ede8cb147620b
Jeff Miller <jeff@humani.se>
2017-10-20 16:11:42 -0700

    update production key

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server "ec2-34-209-236-29.us-west-2.compute.amazonaws.com", user: "ubuntu", roles: %w(web app db)
 
 set :ssh_options,
-  keys: %w(~/.pem/production-nophi.pem),
+  keys: %w(~/.pem/production.jeff.pem),
   forward_agent: false,
   auth_methods: %w(publickey)
