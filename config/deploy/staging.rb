4e2782f179c1cd1d8654a6be70ca6932d884c362
Jeff Miller <jeff@humani.se>
2018-03-22 11:53:53 -0700

    Update EIP for staging, remove specific key depedency

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,5 @@
-server "ec2-54-71-211-16.us-west-2.compute.amazonaws.com", user: "ubuntu", roles: %w(web app db)
+server "35.165.8.120", user: "ubuntu", roles: %w(web app db)
 
 set :ssh_options,
-  keys: %w(~/.pem/cognoa-oregon-staging-ops.pem),
   forward_agent: false,
   auth_methods: %w(publickey)
